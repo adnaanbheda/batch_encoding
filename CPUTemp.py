3f8ad83fc2874fcd3ba36becff8a268bb716dfcf
Adnaan Bheda <adnaan.bheda@gmail.com>
2019-12-03 17:23:50 +0530

    Added Basic Looping Logic.

---
+++ CPUTemp.py
@@ -0,0 +1,48 @@
+import os
+import clr
+import logging
+logging.basicConfig(level=logging.DEBUG)
+openhardwaremonitor_hwtypes = ['Mainboard', 'SuperIO', 'CPU',
+                               'RAM', 'GpuNvidia', 'GpuAti', 'TBalancer', 'Heatmaster', 'HDD']
+
+openhardwaremonitor_sensortypes = ['Voltage', 'Clock', 'Temperature', 'Load',
+                                   'Fan', 'Flow', 'Control', 'Level', 'Factor', 'Power', 'Data', 'SmallData']
+
+
+class CPUTemp:
+    def __init__(self):
+        if os.name == 'nt':
+            file = 'CPUThermometerLib'
+            clr.AddReference(file)
+            from OpenHardwareMonitor import Hardware
+            handle = Hardware.Computer()
+            handle.CPUEnabled = True
+            handle.Open()
+            self.cpu = handle.Hardware[0]
+            self.os = 1
+            logging.debug("Win")
+        else:
+            import psutil
+            self.os = 0
+
+    def fetch_temp(self):
+        self.cpu.Update()
+        for sensor in (self.cpu.Sensors):
+            # Temperature Type Index = 2
+            # CPU Package Value, No need to Average out
+            # if sensor.SensorType == 2:
+            if sensor.Name == "CPU Package":
+                return sensor.Value
+
+    def get_win_cpu_temp(self):
+        logging.debug(self.cpu)
+        return self.fetch_temp()
+
+    def get_linux_cpu_temp(self):
+        pass
+
+    def get_cpu_temp(self):
+        if self.os == 1:
+            return self.get_win_cpu_temp()
+        else:
+            return self.get_linux_cpu_temp()
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
-import sys
+import subprocess
 import argparse
+import multiprocessing
 import pathlib
 import re
 import logging
+import CPUTemp
+import time
+if os.name == 'nt':
+    import wmi
+else:
+    import psutil
+
+cpu = CPUTemp.CPUTemp()
 
 logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser()
@@ -15,6 +24,15 @@ extensions = [".avi", ".mp4", ".mkv", ".m4v"]
 args = parser.parse_args()
 
 
+def work(cmd):
+    return subprocess.call(cmd, shell=False)
+
+
+def check_temp():
+    if cpu.get_cpu_temp() > 65:
+        time.sleep(10)
+
+
 def get_all_files_with_extensions(list_ext=[], dir=""):
     files = []
     for (dirpath, dirnames, filenames) in os.walk(dir):
@@ -24,14 +42,27 @@ def get_all_files_with_extensions(list_ext=[], dir=""):
             _, file_ext = os.path.splitext(str(rel_file))
             if file_ext in list_ext:
                 files.append(rel_file)
+        for d in dirnames:
+            # Make Sure all required directories are created, even if they exist
+            pathlib.Path(args.output_dir, d).mkdir(parents=True, exist_ok=True)
+    # Changing the root of the files as the output dir
+
     return files
 
 
 logging.info(args.input_dir)
 logging.info(args.output_dir)
-files = get_all_files_with_extensions(extensions, args.input_dir)
-logging.debug(files[0])
-files = [pathlib.Path(args.output_dir, x) for x in files]
-logging.debug(files[0])
-for fil in files:
-    logging.debug(fil)
+
+
+original_files = get_all_files_with_extensions(extensions, args.input_dir)
+new_files = [pathlib.Path(args.output_dir, x) for x in files]
+
+# Debug File Values
+if logging.getLevelName == logging.DEBUG:
+    for fil in files:
+        logging.debug(fil)
+
+for i, f in enumerate(original_files):
+    subprocess.call(["handbrake", "-i", f, "-o", new_files[i]])
+    if check_temp() > 70:
+        time.sleep(70)
